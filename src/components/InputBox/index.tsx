c4a37571220cc61daa8f35007595f421688c9777
LYH <dudgk7829@naver.com>
2024-08-26 10:51:36 +0900

    20240826 19:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './style.css';
 
 interface Props {
@@ -7,9 +7,21 @@ interface Props {
     placeholder: string;
     value: string;
     buttonName?: string;
+    
+
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onButtonClick?: () => void;
 }
 
-export default function InputBox({ label, type, placeholder, value, buttonName }: Props) {
+export default function InputBox({ 
+    label, 
+    type, 
+    placeholder, 
+    value, 
+    buttonName, 
+    onChange,
+    onButtonClick
+}: Props) {
 
     // 이름
     // 아이디
@@ -19,8 +31,8 @@ export default function InputBox({ label, type, placeholder, value, buttonName }
         <div className="input-box">
             <div className="label">{label}</div>
             <div className="input-area">
-                <input value={value} type={type} placeholder={placeholder} />
-                {buttonName && <div className="input-button disable">{buttonName}</div>}
+                <input value={value} type={type} placeholder={placeholder} onChange={onChange}/>
+                {buttonName && <div className={`input-button ${value ? 'active' : 'disable'}`} onClick={onButtonClick}>{buttonName}</div>}
             </div>
             <div className="message"></div>
         </div>
