20949999f03bdbc6ff1f1131f587df7a8fa29c76
LYH <dudgk7829@naver.com>
2024-08-26 09:55:20 +0900

    20240826 09:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from 'react';
 import './style.css';
 
 interface Props {
     label: string;
     type: 'text' | 'password';
     placeholder: string;
+    value: string;
     buttonName?: string;
 }
 
-export default function InputBox({ label, type, placeholder, buttonName }: Props) {
-    return (
+export default function InputBox({ label, type, placeholder, value, buttonName }: Props) {
 
+    // 이름
+    // 아이디
+    // 비밀번호
+
+    return (
         <div className="input-box">
             <div className="label">{label}</div>
             <div className="input-area">
-                <input type={type} placeholder={placeholder} />
+                <input value={value} type={type} placeholder={placeholder} />
                 {buttonName && <div className="input-button disable">{buttonName}</div>}
             </div>
             <div className="message"></div>
         </div>
-
     )
+
 }
--- src/views/Auth/index.tsx
+++ src/views/Auth/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react';
 import './style.css';
 import InputBox from 'src/components/InputBox';
 
 export default function Auth() {
 
-    return (
+    const [name, setName] = useState<string>('');
+    const [id, setId] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [passwordCheck, setPasswordCheck] = useState<string>('');
+    const [telNumber, setTelNumber] = useState<string>('');
+    const [authNumber, setAuthNumber] = useState<string>('');
 
-        <div id="auth-wrapper">N
+    const onNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setName(value);
+    };
+    
+    return (
+        <div id="auth-wrapper">
             <div className="auth-image"></div>
             <div className="auth-container">
                 <div style={{ gap: '16px' }} className="auth-box">
@@ -24,23 +35,20 @@ export default function Auth() {
                     <div style={{ width: '64px' }} className="divider"></div>
 
                     <div className="input-container">
-
-                        <InputBox label='이름' type='text' placeholder='이름을 입력해주세요.' />
-                        <InputBox label='아이디' type='text' placeholder='아이디를 입력해주세요.' buttonName='중복확인' />
-                        <InputBox label='비밀번호' type='password' placeholder='비밀번호를 입력해주세요.' />
-                        <InputBox label='비밀번호 확인' type='password' placeholder='비밀번호를 입력해주세요.' />
-                        <InputBox label='전화번호' type='text' placeholder='-빼고 입력해주세요.' buttonName='전화번호 인증' />
-                        <InputBox label='인증번호' type='text' placeholder='인증번호 4자리를 입력해주세요.' buttonName='인증 확인' />
+                        <InputBox value={name} label='이름' type='text' placeholder='이름을 입력해주세요.' />
+                        <InputBox value={id} label='아이디' type='text' placeholder='아이디를 입력해주세요.' buttonName='중복 확인' />
+                        <InputBox value={password} label='비밀번호' type='password' placeholder='비밀번호를 입력해주세요.' />
+                        <InputBox value={passwordCheck} label='비밀번호 확인' type='password' placeholder='비밀번호를 입력해주세요.' />
+                        <InputBox value={telNumber} label='전화번호' type='text' placeholder='-빼고 입력해주세요.' buttonName='전화번호 인증' />
+                        <InputBox value={authNumber} label='인증번호' type='text' placeholder='인증번호 4자리를 입력해주세요.' buttonName='인증 확인' />
                     </div>
 
                     <div className="button-container">
                         <div id="sign-up-button" className="button disable full-width">회원가입</div>
                         <div className="link">로그인</div>
                     </div>
-
                 </div>
             </div>
         </div>
-
     )
 }
