80d7bfee194a5889b9dd4dd46059a69c9fe2140e
LYH <dudgk7829@naver.com>
2024-09-04 12:29:00 +0900

    20240904 12:50

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import './Senicare.css';
 import Auth from 'src/views/Auth';
 import { Route, Routes, useNavigate } from 'react-router';
@@ -8,49 +8,47 @@ import { useCookies } from 'react-cookie';
 // component: root path 컴포넌트 //
 function Index() {
 
-  // state: 쿠키 상태 //
-  const [cookies] = useCookies();
-  
+    // state: 쿠키 상태 //
+    const [cookies] = useCookies();
 
-  // function: 네비게이터 함수 //
-  const navigator = useNavigate();
+    // function: 네비게이터 함수 //
+    const navigator = useNavigate();
 
-  // effect: 마운트 시 경로 이동 effect // 
-  useEffect(() => {
-    if (cookies.accessToken) navigator('/cs');
-    else navigator('/auth');
-    navigator('/auth');
-  }, []);
+    // effect: 마운트 시 경로 이동 effect //
+    useEffect(() => {
+        if (cookies.accessToken) navigator('/cs');
+        else navigator('/auth');
+    }, []);
 
-  // render: root path 컴포넌트 렌더링 //
-  return (
-    <></>
-  );
+    // render: root path 컴포넌트 렌더링 //
+    return (
+        <></>
+    );
 }
 
 // component: Senicare 컴포넌트 //
 export default function Senicare() {
 
-  // render: Senicare 컴포넌트 렌더링 //
-  return (
-    <Routes>
-      <Route index element={<Index />} />
-      <Route path='/auth' element={<Auth />} />
-      <Route path='/cs' element={<MainLayout />}>
-        <Route index element={<>고객 리스트 보기</>} />
-        <Route path='write' element={<>고객 등록</>} />
-        <Route path=':customNumber' element={<>고객 정보 보기</>} />
-        <Route path=':customNumber/update' element={<>고객 정보 수정</>} />
-      </Route>
-      <Route path='/mm' element={<MainLayout />}>
-        <Route index element={<></>} />
-      </Route>
-      <Route path='/hr' element={<MainLayout />}>
-        <Route index element={<></>} />
-        <Route path=':userId' element={<></>} />
-        <Route path=':userId/update' element={<></>} />
-      </Route>
-      <Route path='*' element={<Index />} />
-    </Routes>
-  );
+    // render: Senicare 컴포넌트 렌더링 //
+    return (
+        <Routes>
+            <Route index element={<Index />} />
+            <Route path='/auth' element={<Auth />} />
+            <Route path='/cs' element={<MainLayout />}>
+                <Route index element={<>고객 리스트 보기</>} />
+                <Route path='write' element={<>고객 등록</>} />
+                <Route path=':customNumber' element={<>고객 정보 보기</>} />
+                <Route path=':customNumber/update' element={<>고객 정보 수정</>} />
+            </Route>
+            <Route path='/mm' element={<MainLayout />}>
+                <Route index element={<></>} />
+            </Route>
+            <Route path='/hr' element={<MainLayout />}>
+                <Route index element={<></>} />
+                <Route path=':userId' element={<></>} />
+                <Route path=':userId/update' element={<></>} />
+            </Route>
+            <Route path='*' element={<Index />} />
+        </Routes>
+    );
 }
