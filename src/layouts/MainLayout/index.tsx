1943640c2b3e22583300ba8551f8a53ebd003992
LYH <dudgk7829@naver.com>
2024-09-05 11:53:25 +0900

    20240905 11:50

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router'
 import './style.css';
+import path from 'path';
+import { useCookies } from 'react-cookie';
 
 // component: 로고 컴포넌트 //
 function Logo() {
@@ -20,11 +22,31 @@ function Logo() {
 // component: 상단 컴포넌트 //
 function Top() {
     
+    // state: path 상태 //
+    const { pathname } = useLocation();
+    // state: cookie 상태 //
+    const [cookies, setCookie, removeCookie] = useCookies();
+
+    // variable: 경로 이름 //
+    const path = 
+        pathname.startsWith('/cs') ? '고객 관리' :
+        pathname.startsWith('/mm') ? '용품 관리' :
+        pathname.startsWith('/hr') ? '인사 관리' : '';
+
+    // function: 네이게이터 함수 //
+    const navigator = useNavigate();
+
+    // event handler: 로그아웃 버튼 클릭 이벤트 처리 //
+    const onLogoutButtonClickHandler = () => {
+        removeCookie('accessToken', { path: '/'});
+        navigator('/auth');
+    };
+
     // render: 상단 컴포넌트 렌더링 //
     return (
         <div id='layout-top'>
-            <div className='path'>고객 관리</div>
-            <div className='button second'>로그아웃</div>
+            <div className='path'>{path}</div>
+            <div className='button second' onClick={onLogoutButtonClickHandler}>로그아웃</div>
         </div>
     );
 
@@ -42,6 +64,16 @@ function SideNavigation() {
 
 // component: 메인 레이아웃 컴포넌트 //
 export default function MainLayout() {
+
+    // state: cookie 상태 //
+    const [cookies] = useCookies();
+     // function: 네이게이터 함수 //
+    const navigator = useNavigate();
+
+    // effect: 레이아웃 마운트 시 로그인 여부 확인 //
+    useEffect(() => {
+        if(!cookies.accessToken) navigator('/auth');
+    }, []);
 
     // render: 메인 레이아웃 컴포넌트 렌더링 //
     return (
