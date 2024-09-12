e252ee4a5b03bf5291e3f9fa2e1cf0e92b5477de
LYH <dudgk7829@naver.com>
2024-09-12 15:50:29 +0900

    20240912 15:50

--- src/views/Auth/index.tsx
+++ src/views/Auth/index.tsx
@@ -3,6 +3,8 @@ import './style.css';
 import InputBox from 'src/components/InputBox';
 import axios from 'axios';
 import { error } from 'console';
+import { idCheckRequest } from 'src/apis';
+import { IdCheckRequestDto } from 'src/apis/dto/request/auth';
 
 type AuthPath = '회원가입' | '로그인';
 
@@ -120,17 +122,12 @@ function SignUp({ onPathChange }: AuthComponentProps) {
     const onIdCheckClickHandler = () => {
         if (!id) return;
 
-        axios.post('http://localhost:4000/api/v1/auth/id-check', { userId: id })
-            .then(response => {
-                const { data } = response;
-                console.log('then');
-                console.log(data);
-            })
-            .catch(error => {
-                const { data } = error.response;
-                console.log('catch');
-                console.log(data);
-            });
+        const requestBody: IdCheckRequestDto = {
+            userId: id
+        };
+        idCheckRequest(requestBody).then((responseBody) => {
+            
+        });
 
         // const isDuplicated = id === 'qwer1234';
         // const message = isDuplicated ? '이미 사용중인 아이디입니다.' : '사용 가능한 아이디입니다.';
